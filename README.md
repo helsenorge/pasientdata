71a0552c97d8ebe6d3d6825d72b04079fe337d42
Tanu <tanu-95@hotmail.com>
2019-06-27 13:14:51 +0200

    first commit (fungerende reactapp med googleFit login)
    Koblet til google fitness, henter data og printer ut starttid, sluttid og antal steg for personen som har logget inn. Må lagre på en fornuftig måte og implementere redux kanskje?

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pasientdata
+# PasientData
+
+## About the project
+This is a project done by 5 summer interns, who will find and create a solution for finding and collecting data from devices that patients use. They will do user tests as well as develop a webapp for visualising and storing data with the FHIR standard. 
+
+## How to run
+Run using npm: 
+
+```
+npm start
+```
---
+++ src/pages/homePage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from 'axios'
+
+const API = 'https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.step_count.delta:com.google.android.gms:estimated_steps/datasets/';
+const DEFAULT_QUERY = '631148400000000000-1735686000000000000';
+
+
+class homePage extends Component {
+    constructor(props) {
+        super(props);
+        this.responseGoogle = this.responseGoogle.bind(this);
+        this.state ={
+            name: "",
+            email: "", 
+            pic: "", 
+            dataSet: []
+        }
+    }
+
+    responseGoogle(response){
+        axios.get(API + DEFAULT_QUERY, { headers: { Authorization: "Bearer " + response.accessToken } })
+        .then((res) => {
+            this.setState({ 
+                dataSet: res.data.point,
+                name: response.profileObj.name,
+                email: response.profileObj.email,
+                pic: response.profileObj.imageUrl + '?sz=200'
+            });
+        })
+        .catch((error) => {
+        console.log(error.response);
+        });
+    }
+    
+    render() {
+        const list = this.state.dataSet.map((item, index) => <li key ={index} > {index} {item.startTimeNanos} - {item.endTimeNanos} : {item.value[0].intVal}</li>);
+        return (
+        <div>
+        {
+            <GoogleLogin
+              autoLoad={false}
+              clientId="942269849137-5a1bgivhq71c5ni083igrbss4tbpr6sm.apps.googleusercontent.com"
+              scope='https://www.googleapis.com/auth/fitness.activity.read'
+              approvalPrompt="force"
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
+              responseType="id_token"
+              className="google-login-button"
+              buttonText = "Sign in with you google account"
+            />
+        }
+        <div>
+            {console.log(this.state.dataSet)}
+            {list}
+        </div>
+        </div>
+        );
+    }
+}
+
+export default homePage;
