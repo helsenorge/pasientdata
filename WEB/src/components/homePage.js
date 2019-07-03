9b07843b61134c135b8f5f371c86bdfc844efa47
Tanu <tanu-95@hotmail.com>
2019-07-03 09:54:05 +0200

    Update homepage

--- WEB/src/components/homePage.js
+++ WEB/src/components/homePage.js
@@ -4,10 +4,6 @@ import axios from "axios";
 import moment from "moment";
 import { Redirect } from "react-router";
 
-const API =
-  "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.step_count.delta:com.google.android.gms:estimated_steps/datasets/";
-const DEFAULT_QUERY = "631148400000000000-1735686000000000000";
-
 class HomePage extends Component {
   constructor(props) {
     super(props);
@@ -23,11 +19,49 @@ class HomePage extends Component {
     };
   }
 
+  getUserSteps() {
+    return axios.get(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.step_count.delta:com.google.android.gms:estimated_steps/datasets/631148400000000000-1735686000000000000"
+    );
+  }
+
+  getUserWeight() {
+    return axios.get(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/raw:com.google.weight:com.google.android.apps.fitness:user_input/datasets/631148400000000000-1735686000000000000"
+    );
+  }
+
+  getUserHeight() {
+    return axios.get(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.step_count.delta:com.google.android.gms:estimated_steps/datasets/631148400000000000-1735686000000000000"
+    );
+  }
+
+  getUserHeartBeat() {
+    return axios.get(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.heart_minutes:com.google.android.gms:from_heart_rate<-merge_heart_rate_bpm/datasets/631148400000000000-1735686000000000000"
+    );
+  }
+
+  getUserBloodPressure() {
+    return axios.get(
+      "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.blood_pressure:com.google.android.gms:merged/datasets/631148400000000000-1735686000000000000"
+    );
+  }
+
   responseGoogle(response) {
+    console.log(response);
     axios
-      .get(API + DEFAULT_QUERY, {
-        headers: { Authorization: "Bearer " + response.accessToken }
-      })
+      .all(
+        [
+          this.getUserSteps(),
+          this.getUserWeight(),
+          this.getUserHeight(),
+          this.getUserHeartBeat(),
+          this.getUserBloodPressure()
+        ],
+        { headers: { Authorization: "Bearer " + response.accessToken } }
+      )
       .then(res => {
         let datasets = [...this.state.datasets];
         const pic = response.profileObj.imageUrl + "?sz=200";
@@ -96,7 +130,7 @@ class HomePage extends Component {
             <GoogleLogin
               autoLoad={false}
               clientId="942269849137-5a1bgivhq71c5ni083igrbss4tbpr6sm.apps.googleusercontent.com"
-              scope="https://www.googleapis.com/auth/fitness.activity.read"
+              scope="https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.blood_pressure.read https://www.googleapis.com/auth/fitness.blood_glucose.read https://www.googleapis.com/auth/fitness.body.read"
               approvalPrompt="force"
               onSuccess={this.responseGoogle}
               onFailure={this.responseGoogle}
