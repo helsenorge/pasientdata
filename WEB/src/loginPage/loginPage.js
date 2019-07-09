3e318a291120d5fcfe4bcc4415ef43739bd7dda6
Tanu <tanu-95@hotmail.com>
2019-07-09 12:46:15 +0200

    merge solved

--- WEB/src/loginPage/loginPage.js
+++ WEB/src/loginPage/loginPage.js
@@ -9,12 +9,6 @@ class LoginPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // googleId: "",
-      // firstname: "",
-      // lastname: "",
-      // email: "",
-      // image: "",
-      // datasets: [],
       redirectProfile: false
     };
   }
@@ -58,7 +52,6 @@ class LoginPage extends Component {
                 responseType="id_token"
                 className="google-login-button"
                 buttonText="Sign in with you google account"
-                //onLogin={this.props.onLogin}
               />
             }
           </div>
