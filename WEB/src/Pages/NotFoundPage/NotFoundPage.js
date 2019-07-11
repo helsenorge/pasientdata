698b9c21f37461a33ec7fad18b44550fb1e1b6b8
Tanu <tanu-95@hotmail.com>
2019-07-11 15:15:11 +0200

    Small path fix in NotFoundPage

--- WEB/src/Pages/NotFoundPage/NotFoundPage.js
+++ WEB/src/Pages/NotFoundPage/NotFoundPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PageNotFound from "../../images/medError.jpg";
+import PageNotFound from "../../Images/medError.jpg";
 import "./NotFoundPage.css";
 
 class NotFoundPage extends React.Component {
