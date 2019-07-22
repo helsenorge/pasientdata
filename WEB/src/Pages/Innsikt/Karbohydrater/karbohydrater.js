88122440b71252fa92d77ec29bcf79c5c52775a5
adriabs <adrianskibelid@hotmail.com>
2019-07-22 10:42:22 +0200

    Added mock cards for innsikt pages

--- WEB/src/Pages/Innsikt/Karbohydrater/karbohydrater.js
+++ WEB/src/Pages/Innsikt/Karbohydrater/karbohydrater.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import NavigationBar from "../../../components/NavigationBar/navigationBar.js";
+import OversiktKort from "../../../components/InnsiktsKort/oversiktKort";
+import GrafKort from "../../../components/InnsiktsKort/grafKort";
+import MonsterKort from "../../../components/InnsiktsKort/monsterKort";
 
 class Karbohydrater extends Component {
   state = {};
@@ -7,7 +10,9 @@ class Karbohydrater extends Component {
     return (
       <div>
         <NavigationBar />
-        <div>Karbohydrater</div>
+        <OversiktKort datatype="Karbohydrater" />
+        <GrafKort datatype="Karbohydrater" />
+        <MonsterKort datatype="Karbohydrater" />
       </div>
     );
   }
--- WEB/src/Pages/Innsikt/Skritt/skritt.js
+++ WEB/src/Pages/Innsikt/Skritt/skritt.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import NavigationBar from "../../../components/NavigationBar/navigationBar.js";
+import OversiktKort from "../../../components/InnsiktsKort/oversiktKort";
+import GrafKort from "../../../components/InnsiktsKort/grafKort";
+import MonsterKort from "../../../components/InnsiktsKort/monsterKort";
 
 class Skritt extends Component {
   state = {};
@@ -7,7 +10,9 @@ class Skritt extends Component {
     return (
       <div>
         <NavigationBar />
-        <div>Skritt</div>
+        <OversiktKort datatype="Skritt" />
+        <GrafKort datatype="Skritt" />
+        <MonsterKort datatype="Skritt" />
       </div>
     );
   }
