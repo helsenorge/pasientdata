00ee8bbbe731fa574826456f9f512e14ddc6892b
adriabs <adrianskibelid@hotmail.com>
2019-07-23 15:49:13 +0200

    Cleaned barplotter, separating out util functions

--- WEB/src/components/Barplotter/barPlotter.js
+++ WEB/src/components/Barplotter/barPlotter.js
@@ -9,6 +9,7 @@ import {
   Bar,
   ResponsiveContainer
 } from "recharts";
+import aggregateData from "../../Utils/aggregateData";
 
 /*
  * The BarPlotter component.
@@ -24,144 +25,14 @@ import {
  */
 
 class BarPlotter extends Component {
-  findStartAndEndIndex = () => {
-    const length = this.props.data.length;
-    const end = this.props.end;
-    const start = this.props.start;
-
-    let startIndex = 0;
-    let endIndex = length - 1;
-    let endIndexFound = false;
-
-    if (
-      moment(end, "YYYY-MM-DDTHH:mm:ss").isAfter(
-        moment(this.props.data[length - 1].start, "YYYY-MM-DDTHH:mm:ss")
-      )
-    ) {
-      endIndexFound = true;
-    }
-
-    for (let i = 0; i < length; i++) {
-      if (
-        moment(start, "YYYY-MM-DDTHH:mm:ss").isAfter(
-          moment(this.props.data[i].start, "YYYY-MM-DDTHH:mm:ss")
-        )
-      ) {
-        startIndex = i; // Index right before the first data point we want to include.
-      }
-      if (
-        !endIndexFound &&
-        moment(end, "YYYY-MM-DDTHH:mm:ss").isBefore(
-          moment(this.props.data[i].start, "YYYY-MM-DDTHH:mm:ss")
-        )
-      ) {
-        endIndex = i; // Index right after the first data point we want to include.
-        break;
-      }
-    }
-    if (!endIndexFound) {
-      endIndex--;
-    }
-    if (startIndex !== length - 1) {
-      startIndex++;
-    }
-
-    return { startIndex: startIndex, endIndex: endIndex };
-  };
-
-  aggregateData = () => {
-    const { startIndex, endIndex } = this.findStartAndEndIndex();
-    // console.log("StartIndex = ", startIndex);
-    // console.log("EndIndex = ", endIndex);
-    let slicedData = this.props.data.slice(startIndex, endIndex);
-
-    const inputFormat = "YYYY-MM-DDTHH:mm:ss";
-    const interval = this.props.interval;
-    const startTime = moment(this.props.start, inputFormat);
-    const endTime = moment(this.props.end, inputFormat);
-    const slicedLength = slicedData.length;
-    const outputFormat = this.props.outputFormat;
-
-    /*
-     * Loop through the desired dataset and aggregate
-     */
-
-    let aggregated = [];
-    let data = slicedData.map(item => ({ x: item.start, y: item.value }));
-    if (data === undefined || data.length === 0) {
-      data.push({ y: 0, x: startTime.format(inputFormat) });
-    }
-    let sum = data[0].y;
-    let start = moment(data[0].x, inputFormat).startOf(interval);
-
-    // Add empty bars at start if needed
-    let added = 1;
-    while (moment(start).diff(startTime, interval + "s") - added > -1) {
-      aggregated.push({
-        y: 0,
-        x: moment(startTime)
-          .add(added, interval + "s")
-          .format(outputFormat)
-      });
-      added++;
-    }
-
-    let currentDataTime;
-    for (let i = 1; i < slicedLength; i++) {
-      currentDataTime = moment(data[i].x, inputFormat);
-      if (
-        moment(start).diff(
-          currentDataTime.startOf(interval),
-          interval + "s"
-        ) === 0
-      ) {
-        sum += data[i].y;
-      } else {
-        // Add empty bars inbetween if needed
-        let skipped = 0;
-        aggregated.push({
-          y: sum,
-          x: start.format(outputFormat)
-        });
-        while (
-          moment(start).diff(currentDataTime, interval + "s") + skipped <
-          -1
-        ) {
-          aggregated.push({
-            y: 0,
-            x: moment(start)
-              .add(1 + skipped, interval + "s")
-              .startOf(interval)
-              .format(outputFormat)
-          });
-          skipped++;
-        }
-
-        sum = data[i].y;
-        start = currentDataTime.startOf(interval);
-      }
-    }
-    aggregated.push({ y: sum, x: start.format(outputFormat) });
-
-    // Add empty bars at end if needed
-    while (
-      moment(endTime).diff(start.add(1, interval + "s"), interval + "s") > 0
-    ) {
-      aggregated.push({ y: 0, x: start.format(outputFormat) });
-    }
-    if (
-      moment(endTime).diff(start.add(1, interval + "s"), interval + "s") > -1
-    ) {
-      aggregated.push({
-        y: 0,
-        x: start.subtract(1, interval + "s").format(outputFormat)
-      });
-    }
-    return aggregated;
-  };
-
   render() {
-    let aggregated = this.aggregateData();
+    let aggregated = aggregateData(
+      this.props.data,
+      this.props.interval,
+      this.props.start,
+      this.props.end,
+      this.props.outputFormat
+    );
 
     if (this.props.page === "Dashboard") {
       return (
--- WEB/src/components/InsightCards/trendGoalsCard.js
+++ WEB/src/components/InsightCards/trendGoalsCard.js
@@ -4,119 +4,182 @@ import FakeGlucoseData from "../../Utils/fakeGlucose";
 import Trends from "../../Utils/trends";
 import { PieChart, Pie, Cell, Label, ResponsiveContainer } from "recharts";
 import "./trendGoalsCard.css";
+import { connect } from "react-redux";
 
 class Oversiktkort extends Component {
   oversiktContent = () => {
-    if (this.props.datatype === "Blodsukker") {
-      const data = FakeGlucoseData();
-      const upperLimit = 12;
-      const lowerLimit = 5;
+    let data = FakeGlucoseData();
+    let upperLimit = 12;
+    let lowerLimit = 5;
+    let trendValue = 2;
+    let goalValue = 75;
+    let { mean, timeAbove, timeWithin, timeBelow } = Trends(
+      data,
+      upperLimit,
+      lowerLimit
+    );
+    let currentValue =
+      (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
+    let unit = "%";
+    let trends;
+    switch (this.props.datatype) {
+      case "Blodsukker":
+        data = FakeGlucoseData();
+        upperLimit = 12;
+        lowerLimit = 5;
+        trendValue = 2;
+        goalValue = 75;
+        trends = Trends(data, upperLimit, lowerLimit);
+        mean = trends.mean;
+        timeAbove = trends.timeAbove;
+        timeWithin = trends.timeWithin;
+        timeBelow = trends.timeBelow;
+        currentValue =
+          (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
+        unit = "%";
+        console.log("Blodsukker");
+        break;
+      case "Insulin":
+        data = this.props.patient.datasets[0].measurements;
+        upperLimit = 20;
+        lowerLimit = 5;
+        trendValue = 2;
+        goalValue = 75;
+        trends = Trends(data, upperLimit, lowerLimit);
+        mean = trends.mean;
+        timeAbove = trends.timeAbove;
+        timeWithin = trends.timeWithin;
+        timeBelow = trends.timeBelow;
+        currentValue =
+          (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
+        unit = "%";
+        break;
+      case "Skritt":
+        data = this.props.patient.datasets[0].measurements;
+        upperLimit = 1000000;
+        lowerLimit = 2000;
+        trendValue = 200;
+        goalValue = 2000;
 
-      const { mean, timeAbove, timeWithin, timeBelow } = Trends(
-        data,
-        upperLimit,
-        lowerLimit
-      );
-      const COLORS = ["#A61E7B", "#569B7E", "#E38B21"];
-      const goalArrowPic = require("../../Images/goalArrow.svg");
-      const downTrianglePic = require("../../Images/downTriangle.svg");
-      const trendValue = 2;
-      const currentValue =
-        (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
-      const goalValue = 75;
-      return (
-        <div className="flex-container-trend-goals outer-div-trend-goals">
-          <ResponsiveContainer
-            className="flex-children-trend-goals"
-            width={250}
-            height={180}
-          >
-            <PieChart width={1000} height={500}>
-              <Pie
-                data={[
-                  { value: timeBelow, name: "Time below" },
-                  { value: timeWithin, name: "Time within" },
-                  { value: timeAbove, name: "Time above" }
-                ]}
-                dataKey="value"
-                nameKey="name"
-                startAngle={-30}
-                endAngle={+210}
-                innerRadius={60}
-                outerRadius={80}
-                fill="#979797"
-                label={({
-                  cx,
-                  cy,
-                  midAngle,
-                  innerRadius,
-                  outerRadius,
-                  value,
-                  index
-                }) => {
-                  const RADIAN = Math.PI / 180;
-                  const radius = 10 + innerRadius + (outerRadius - innerRadius);
-                  let x = cx + radius * Math.cos(-midAngle * 2 * RADIAN);
-                  let y = cy + radius * Math.sin(-midAngle * 2 * RADIAN);
-                  let returnString = "";
-                  if (index === 2) {
-                    returnString = upperLimit;
-                    x = cx + radius * Math.cos(-midAngle * 1.955 * RADIAN);
-                    y = cy + radius * Math.sin(-midAngle * 1.955 * RADIAN);
-                  } else if (index === 1) {
-                    returnString = lowerLimit;
-                    x = cx + radius * Math.cos(-midAngle * 2 * RADIAN);
-                    y = cy + radius * Math.sin(-midAngle * 2 * RADIAN);
-                  }
-                  return (
-                    <text
-                      x={x}
-                      y={y}
-                      fill="#8884d8"
-                      textAnchor={x > cx ? "start" : "end"}
-                      dominantBaseline="end"
-                    >
-                      {returnString}
-                    </text>
-                  );
-                }}
-                labelLine={false}
-              >
-                {data.map((entry, index) => (
-                  <Cell key="" fill={COLORS[index % COLORS.length]} />
-                ))}{" "}
-                /> <Label value={currentValue + "%"} position="center" />
-              </Pie>
-            </PieChart>
-          </ResponsiveContainer>
-          <div className="flex-children-trend-goals flex-side-container-trend-goals">
-            <div className="flex-children-trend-goals flex-container-trend-goals upper-trend-goals-div">
-              <img
-                src={goalArrowPic}
-                alt={"logo"}
-                className="index-Image flex-children-trend-goals trend-pic"
+        trends = Trends(data, upperLimit, lowerLimit);
+        mean = trends.mean;
+        timeAbove = trends.timeAbove;
+        timeWithin = trends.timeWithin;
+        timeBelow = trends.timeBelow;
+        currentValue = mean;
+        unit = "%";
+        break;
+      case "Vekt":
+        break;
+      case "Blodtrykk":
+        break;
+      case "Karbohydrater":
+        break;
+      default:
+    }
+
+    const COLORS = ["#A61E7B", "#569B7E", "#E38B21"];
+    const goalArrowPic = require("../../Images/goalArrow.svg");
+    const downTrianglePic = require("../../Images/downTriangle.svg");
+    return (
+      <div className="flex-container-trend-goals outer-div-trend-goals">
+        <ResponsiveContainer
+          className="flex-children-trend-goals"
+          width={250}
+          height={180}
+        >
+          <PieChart width={1000} height={500}>
+            <Pie
+              data={[
+                { value: timeBelow, name: "Time below" },
+                { value: timeWithin, name: "Time within" },
+                { value: timeAbove, name: "Time above" }
+              ]}
+              dataKey="value"
+              nameKey="name"
+              startAngle={-30}
+              endAngle={+210}
+              innerRadius={60}
+              outerRadius={80}
+              fill="#979797"
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index
+              }) => {
+                const RADIAN = Math.PI / 180;
+                const radius = 10 + innerRadius + (outerRadius - innerRadius);
+                let x = cx + radius * Math.cos(-midAngle * 2 * RADIAN);
+                let y = cy + radius * Math.sin(-midAngle * 2 * RADIAN);
+                let returnString = "";
+                if (index === 2) {
+                  returnString = upperLimit;
+                  x = cx + radius * Math.cos(-midAngle * 1.955 * RADIAN);
+                  y = cy + radius * Math.sin(-midAngle * 1.955 * RADIAN);
+                } else if (index === 1) {
+                  returnString = lowerLimit;
+                  x = cx + radius * Math.cos(-midAngle * 2 * RADIAN);
+                  y = cy + radius * Math.sin(-midAngle * 2 * RADIAN);
+                }
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    fill="#8884d8"
+                    textAnchor={x > cx ? "start" : "end"}
+                    dominantBaseline="end"
+                  >
+                    {returnString}
+                  </text>
+                );
+              }}
+              labelLine={false}
+            >
+              {data.map((entry, index) => (
+                <Cell key="" fill={COLORS[index % COLORS.length]} />
+              ))}{" "}
+              />{" "}
+              <Label
+                value={Math.floor(currentValue) + unit}
+                position="center"
               />
-              <div className="flex-children-trend-goals flex-side-container-trend-goals">
-                <div className="flex-children-trend-goals">Mål:</div>
-                <div className="flex-children-trend-goals">{goalValue} %</div>
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+        <div className="flex-children-trend-goals flex-side-container-trend-goals">
+          <div className="flex-children-trend-goals flex-container-trend-goals upper-trend-goals-div">
+            <img
+              src={goalArrowPic}
+              alt={"logo"}
+              className="index-Image flex-children-trend-goals trend-pic"
+            />
+            <div className="flex-children-trend-goals flex-side-container-trend-goals">
+              <div className="flex-children-trend-goals">Mål:</div>
+              <div className="flex-children-trend-goals">
+                {goalValue} {unit}
               </div>
             </div>
-            <div className="flex-children-trend-goals flex-container-trend-goals lower-trend-goals-div">
-              <img
-                src={downTrianglePic}
-                alt={"logo"}
-                className="index-Image flex-children trend-pic"
-              />
-              <div className="flex-children-trend-goals flex-side-container-trend-goals">
-                <div className="flex-children-trend-goals">Trend:</div>
-                <div className="flex-children-trend-goals">{trendValue} %</div>
+          </div>
+          <div className="flex-children-trend-goals flex-container-trend-goals lower-trend-goals-div">
+            <img
+              src={downTrianglePic}
+              alt={"logo"}
+              className="index-Image flex-children trend-pic"
+            />
+            <div className="flex-children-trend-goals flex-side-container-trend-goals">
+              <div className="flex-children-trend-goals">Trend:</div>
+              <div className="flex-children-trend-goals">
+                {trendValue} {unit}
               </div>
             </div>
           </div>
         </div>
-      );
-    }
-    return "Under construction...";
+      </div>
+    );
   };
 
   render() {
@@ -126,4 +189,11 @@ class Oversiktkort extends Component {
   }
 }
 
-export default Oversiktkort;
+function mapStateToProps(state) {
+  return {
+    patient: state.patient,
+    baseInfo: state.baseInfo
+  };
+}
+
+export default connect(mapStateToProps)(Oversiktkort);
