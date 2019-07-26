a0909e0cb805895da141cb49548ed031ad2144fc
Tanu <tanu-95@hotmail.com>
2019-07-26 13:37:51 +0200

    Styled goalContent for all devices as well as started with arrowPlot

--- WEB/src/components/GoalContent/goalContent.js
+++ WEB/src/components/GoalContent/goalContent.js
@@ -15,24 +15,26 @@ import {
 import CardComponent from "../../components/Card/cardComponent";
 import FakeGlucoseData from "../../Utils/fakeGlucose";
 import Trends from "../../Utils/trends";
+import periodFromView from "../../Utils/periodFromView";
+import aggregateData from "../../Utils/aggregateData";
 
 class GoalContent extends Component {
   displayUnit = () => {
     switch (this.props.datatype) {
       case "Blodsukker":
-        return "%";
+        return "/dag";
       case "BlodsukkerAvg":
         return "mmol/l";
       case "Skritt":
         return "skritt";
       case "Vekt":
-        return "kg";
+        return "kilogram";
       case "FysiskAktivitet":
-        return "min";
+        return "min/uke";
       case "Karbohydrater":
-        return "g";
+        return "gram/dag";
       case "Blodtrykk":
-        return "mmHg";
+        return "mmHg/dag";
       default:
         return "Default";
     }
@@ -49,60 +51,225 @@ class GoalContent extends Component {
       { value: 1 },
       { value: 1 }
     ];
-    let goalText;
+    let goal;
+    let data = FakeGlucoseData();
+    let upperLimit = 12;
+    let lowerLimit = 5;
+    let trendValue = 2;
+    let goalValue = 75;
+    let { mean, timeAbove, timeWithin, timeBelow } = Trends(
+      data,
+      upperLimit,
+      lowerLimit
+    );
+    let currentValue =
+      (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
+    let unit = "%";
+    let trends;
+    let hasUpperLimit = true;
+    let percentGoal;
+    let upperGoal = 80;
+    let lowerGoal = 70;
+    let pieSideSize = 20;
+    let { periodName, periodNumber, intervalName } = periodFromView(
+      this.props.baseInfo.view
+    );
 
     switch (this.props.datatype) {
       case "Blodsukker":
         COLORS = generalColors;
-        // data = FakeGlucoseData();
-        // upperLimit = 12;
-        // lowerLimit = 5;
-        // percentGoal = 65;
-        // trendValue = 2;
-        // goalValue = 85;
-        // trends = Trends(data, upperLimit, lowerLimit);
-        // mean = trends.mean;
-        // timeAbove = trends.timeAbove;
-        // timeWithin = trends.timeWithin;
-        // timeBelow = trends.timeBelow;
-        // currentValue =
-        //   (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
-        // unit = "%";
-        // hasUpperLimit = false;
+        goal = "80%";
+        data = FakeGlucoseData();
+        upperLimit = 14;
+        lowerLimit = 5;
+        percentGoal = 65;
+        trendValue = 2;
+        goalValue = 85;
+        trends = Trends(data, upperLimit, lowerLimit);
+        mean = trends.mean;
+        timeAbove = trends.timeAbove;
+        timeWithin = trends.timeWithin;
+        timeBelow = trends.timeBelow;
+        currentValue =
+          (timeWithin * 100) / (timeAbove + timeWithin + timeBelow);
+        unit = "%";
+        hasUpperLimit = false;
         break;
       case "BlodsukkerAvg":
         COLORS = generalColors;
+        goal = 7;
         break;
       case "Skritt":
         COLORS = physicalActiveColors;
         dataSet = [{ value: 1 }, { value: 2 }, { value: 2 }];
+        goal = this.props.patient.goals[0].value;
+        data = this.props.patient.datasets[0].measurements;
+        upperLimit = 1000000;
+        lowerLimit = 10000;
+        trendValue = 200;
+        goalValue = 15000;
+        let aggregated = aggregateData(
+          data,
+          intervalName,
+          moment()
+            .subtract(periodNumber, periodName)
+            .format("YYYY-MM-DDTHH:mm:ss"),
+          moment().format("YYYY-MM-DDTHH:mm:ss"),
+          "ddd"
+        );
+        trends = Trends(aggregated, upperLimit, lowerLimit);
+        mean = trends.mean;
+        timeAbove = trends.timeAbove;
+        timeWithin = trends.timeWithin;
+        timeBelow = trends.timeBelow;
+        currentValue = mean;
+        unit = " skritt";
+        hasUpperLimit = false;
+        pieSideSize = 2000;
         break;
       case "Vekt":
         COLORS = generalColors;
+        goal = 65;
         break;
       case "FysiskAktivitet":
         COLORS = physicalActiveColors;
         dataSet = [{ value: 1 }, { value: 2 }, { value: 2 }];
+        goal = 90;
         break;
       case "Karbohydrater":
         COLORS = generalColors;
+        goal = 280;
         break;
       case "Blodtrykk":
         COLORS = generalColors;
+        goal = "number?";
         break;
       default:
         return;
     }
+
+    let angles = [];
+    let pieData;
+    let lowerTextValue;
+    let upperTextValue;
+    let lowerText;
+    let upperText;
+    let goalText;
+    if (hasUpperLimit) {
+      if (unit === "%") {
+        pieData = [
+          { value: Math.min(pieSideSize, 100 - upperGoal) },
+          { value: upperGoal - lowerGoal },
+          { value: Math.min(pieSideSize, lowerGoal) }
+        ];
+        lowerTextValue = Math.max(0, lowerGoal - pieSideSize);
+        upperTextValue = Math.min(100, upperGoal + pieSideSize);
+        goalText = lowerGoal + unit + " - " + upperGoal + unit;
+      } else {
+        pieData = [
+          { value: pieSideSize },
+          { value: upperGoal - lowerGoal },
+          {
+            value: Math.min(
+              currentValue - pieSideSize,
+              lowerGoal - pieSideSize
+            ),
+            name: "Time Below"
+          }
+        ];
+      }
+      angles = [
+        ((-30 +
+          (pieData[0].value /
+            (pieData[0].value + pieData[1].value + pieData[2].value)) *
+            240) *
+          Math.PI) /
+          180,
+        ((-30 +
+          ((pieData[0].value + pieData[1].value) /
+            (pieData[0].value + pieData[1].value + pieData[2].value)) *
+            240) *
+          Math.PI) /
+          180,
+        0
+      ];
+    } else {
+      // only lower limit
+      if (unit === "%") {
+        pieData = [
+          { value: Math.min(pieSideSize, 100 - goalValue) },
+          { value: Math.min(pieSideSize, goalValue) }
+        ];
+        lowerTextValue = Math.max(0, goalValue - pieSideSize);
+        upperTextValue = Math.min(100, goalValue + pieSideSize);
+        goalText = goalValue + unit;
+      } else {
+        pieData = [{ value: pieSideSize }, { value: pieSideSize }];
+        lowerTextValue = Math.max(0, goalValue - pieSideSize);
+        upperTextValue = goalValue + pieSideSize;
+      }
+      angles = [
+        0,
+        ((-30 +
+          (pieData[0].value / (pieData[0].value + pieData[1].value)) * 240) *
+          Math.PI) /
+          180,
+        0
+      ];
+
+      goalText = goalValue + " " + unit;
+    }
+    lowerText = lowerTextValue + unit;
+    upperText = upperTextValue + unit;
+
+    let arrowAngle;
+    let arrowOutsideRangeSpacing = 5;
+
+    if (currentValue < lowerTextValue) {
+      arrowAngle = ((210 + arrowOutsideRangeSpacing) * Math.PI) / 180;
+    } else if (currentValue > upperTextValue) {
+      arrowAngle = ((-30 - arrowOutsideRangeSpacing) * Math.PI) / 180;
+    } else {
+      arrowAngle =
+        ((-30 +
+          ((upperTextValue - currentValue) /
+            (upperTextValue - lowerTextValue)) *
+            240) *
+          Math.PI) /
+        180;
+    }
+
+    let triangleAngle = (70 * Math.PI) / 180; // går og litt på bredde
+    let r = 20; // lengde pil
+    let theta = 9; // ish bredde
+    let radius = 40; // hvor langt unna center
+
+    let centerX = 125 + radius * Math.cos(-arrowAngle);
+    let centerY = 110 + radius * Math.sin(-arrowAngle);
+    let x1 = Math.floor(centerX + r * Math.cos(-arrowAngle));
+    let y1 = Math.floor(centerY + r * Math.sin(-arrowAngle));
+    let x2 = Math.floor(
+      centerX + theta * Math.cos(-arrowAngle - triangleAngle)
+    );
+    let y2 = Math.floor(
+      centerY + theta * Math.sin(-arrowAngle - triangleAngle)
+    );
+    let x3 = Math.floor(
+      centerX + theta * Math.cos(-arrowAngle + triangleAngle)
+    );
+    let y3 = Math.floor(
+      centerY + theta * Math.sin(-arrowAngle + triangleAngle)
+    );
+
+    let pointString = x1 + " " + y1 + " " + x2 + " " + y2 + " " + x3 + " " + y3;
 
     return (
       <div className="flex-container-trend-goals outer-div-trend-goals">
         <div className="split">
           <div className="circleBound">
             <div className="goalText">Mål:</div>
-            <div className="goalPercentText">
-              {this.props.patient.goals[0].value}
-            </div>
-            <div className="goalText">/dag</div>
+            <div className="goalPercentText">{goal}</div>
+            <div className="unitText">{this.displayUnit()}</div>
           </div>
         </div>
         <div className="row split">
@@ -126,8 +293,18 @@ class GoalContent extends Component {
                   {dataSet.map((entry, index) => (
                     <Cell key="" fill={COLORS[index % COLORS.length]} />
                   ))}
-                  <Label value={"status:"} position="center" />
+                  <Label
+                    value={Math.floor(currentValue) + unit}
+                    position="center"
+                  />
                 </Pie>
+                <svg>
+                  <polygon
+                    points={pointString}
+                    fill="#4F4F4F"
+                    className="trend-polygon"
+                  />
+                </svg>
               </PieChart>
             </ResponsiveContainer>
           </div>
@@ -159,33 +336,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(GoalContent);
-
-// let triangleAngle = (70 * Math.PI) / 180; // går og litt på bredde
-// let r = 20; // lengde pil
-// let theta = 9; // ish bredde
-// let radius = 40; // hvor langt unna center
-
-// let centerX = 125 + radius * Math.cos(-arrowAngle);
-// let centerY = 110 + radius * Math.sin(-arrowAngle);
-// let x1 = Math.floor(centerX + r * Math.cos(-arrowAngle));
-// let y1 = Math.floor(centerY + r * Math.sin(-arrowAngle));
-// let x2 = Math.floor(
-//   centerX + theta * Math.cos(-arrowAngle - triangleAngle)
-// );
-// let y2 = Math.floor(
-//   centerY + theta * Math.sin(-arrowAngle - triangleAngle)
-// );
-// let x3 = Math.floor(
-//   centerX + theta * Math.cos(-arrowAngle + triangleAngle)
-// );
-// let y3 = Math.floor(
-//   centerY + theta * Math.sin(-arrowAngle + triangleAngle)
-// );
-
-// <svg>
-// <polygon
-//   points={pointString}
-//   fill="#4F4F4F"
-//   className="trend-polygon"
-// />{" "}
-// </svg>
