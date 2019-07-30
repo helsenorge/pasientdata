70814ca8299f4d313fa70057f55724142d37a00f
adriabs <adrianskibelid@hotmail.com>
2019-07-30 12:52:38 +0200

    Fixed more styling of trendgoalscard

--- WEB/src/components/InsightCards/trendGoalsCard.js
+++ WEB/src/components/InsightCards/trendGoalsCard.js
@@ -270,7 +270,7 @@ class TrendGoalsCard extends Component {
     let triangleAngle = (70 * Math.PI) / 180;
     let r = 20;
     let theta = 9;
-    let radius = 40;
+    let radius = 60;
 
     let centerX = 125 + radius * Math.cos(-arrowAngle);
     let centerY = 110 + radius * Math.sin(-arrowAngle);
@@ -305,8 +305,8 @@ class TrendGoalsCard extends Component {
               nameKey="name"
               startAngle={-30}
               endAngle={+210}
-              innerRadius={60}
-              outerRadius={80}
+              innerRadius={75}
+              outerRadius={90}
               fill="#979797"
               label={({ cx, cy, index }) => {
                 let returnString = "";
@@ -328,9 +328,9 @@ class TrendGoalsCard extends Component {
                 return (
                   <React.Fragment>
                     <text
-                      x={cx + 90 * Math.cos(-angles[index])}
-                      y={cy + 90 * Math.sin(-angles[index])}
-                      fill="#8884d8"
+                      x={cx + 95 * Math.cos(-angles[index])}
+                      y={cy + 95 * Math.sin(-angles[index])}
+                      fill="#569B7E"
                       textAnchor="middle"
                       dominantBaseline="end"
                       className="caption"
@@ -338,9 +338,9 @@ class TrendGoalsCard extends Component {
                       {returnString}
                     </text>
                     <text
-                      x={cx + 100 * Math.cos((-210 * Math.PI) / 180)}
-                      y={cy + 100 * Math.sin((-210 * Math.PI) / 180)}
-                      fill="#8884d8"
+                      x={cx + 110 * Math.cos((-210 * Math.PI) / 180)}
+                      y={cy + 110 * Math.sin((-210 * Math.PI) / 180)}
+                      fill="#7D8081"
                       textAnchor="middle"
                       dominantBaseline="end"
                       className="caption"
@@ -348,14 +348,34 @@ class TrendGoalsCard extends Component {
                       {lowerText}
                     </text>
                     <text
-                      x={cx + 100 * Math.cos((30 * Math.PI) / 180)}
-                      y={cy + 100 * Math.sin((30 * Math.PI) / 180)}
-                      fill="#8884d8"
+                      x={cx + 110 * Math.cos((30 * Math.PI) / 180)}
+                      y={cy + 110 * Math.sin((30 * Math.PI) / 180)}
+                      fill="#7D8081"
                       textAnchor="middle"
                       dominantBaseline="end"
                       className="caption"
                     >
                       {upperText}
+                    </text>
+                    <text
+                      x={cx}
+                      y={cy + 10}
+                      fill="#000000"
+                      textAnchor="middle"
+                      dominantBaseline="end"
+                      className="caption" // "distance-to-goal-text"
+                    >
+                      {goalText - Math.floor(currentValue)} skritt fra målet
+                    </text>
+                    <text
+                      x={cx}
+                      y={cy - 5}
+                      fill="#000000"
+                      textAnchor="middle"
+                      dominantBaseline="end"
+                      className="large-numerical-value"
+                    >
+                      {Math.floor(currentValue) + unit}
                     </text>
                   </React.Fragment>
                 );
@@ -366,10 +386,11 @@ class TrendGoalsCard extends Component {
                 <Cell key="" fill={COLORS[index % COLORS.length]} />
               ))}{" "}
               />{" "}
-              <Label
+              {/* <Label
                 value={Math.floor(currentValue) + unit}
                 position="center"
-              />
+                className="large-numerical-value"
+              /> */}
             </Pie>
             <svg>
               <polygon
