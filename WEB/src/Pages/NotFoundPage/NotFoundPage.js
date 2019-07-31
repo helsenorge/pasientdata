787f6c4e11413c48509996b821170cfff93e7c59
adriabs <adrianskibelid@hotmail.com>
2019-07-31 13:55:05 +0200

    Description of 404 page

--- WEB/src/Pages/NotFoundPage/NotFoundPage.js
+++ WEB/src/Pages/NotFoundPage/NotFoundPage.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import PageNotFound from "../../Images/medError.jpg";
 import "./NotFoundPage.css";
 
-class NotFoundPage extends React.Component {
+/*
+ * Simple 404 page.
+ */
 
+class NotFoundPage extends React.Component {
   render() {
     return (
       <div className="text">
