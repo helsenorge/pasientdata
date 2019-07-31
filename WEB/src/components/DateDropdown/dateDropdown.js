2cf25c8c3f686217d38879984abfc5f91341386e
adriabs <adrianskibelid@hotmail.com>
2019-07-31 13:46:15 +0200

    removed old comment

--- WEB/src/components/DateDropdown/dateDropdown.js
+++ WEB/src/components/DateDropdown/dateDropdown.js
@@ -31,7 +31,7 @@ class DateDropdown extends Component {
   render() {
     return (
       <FormSelect
-        onChange={e => this.props.setView(e.target.value)} // Change to a set state or smth when u want to actually use
+        onChange={e => this.props.setView(e.target.value)}
         className="custom-form-select"
         defaultValue={this.props.baseInfo.view}
       >
